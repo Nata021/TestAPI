fbd788477c26b31ebff04fa55f4015ecf3f684c9
Nata021 <96535846+Nata021@users.noreply.github.com>
2025-05-28 18:47:50 +0300

    Update Tests

--- ClickUp_JS/cypress/e2e/1-getting-started/todo.cy.js
+++ ClickUp_JS/cypress/e2e/1-getting-started/todo.cy.js
@@ -1,54 +1,39 @@
 import { faker } from '@faker-js/faker';
 
 
- describe('Test goals on clickup', () => {
-//     //GET запит
-//     it('Should send GET request and return 200', () => {
-//         cy.sentRequest('/team/90151137047/goal', 'GET').then((response) => {
-//             expect(response.status).to.equal(200);
-//             expect(response.body.goals[0].team_id).to.equal('90151137047');
-//         })
-//     })
-    //Post запит
-    // it('Should send POST request with valid body from file and return 200', () => {
-    //     cy.fixture('example.json').then((content) => {
-    //         content.name = faker.internet.email();
-    //         cy.createGoalPositive(content).then((response) => {
-    //             cy.log(response.duration)
-    //             expect(response.status).to.equal(200)
-    //         })
-    //     })
-    // })
+describe('Goal CRUD API Test Suite', () => {
+    it('should create, verify, update and delete a goal', () => {
+        // 1. Створити ціль
+        cy.createGoal().then(createResponse => {
+            cy.log('Goal created:', createResponse.body);
+        });
 
-    //PUT запит
-  //it('Should send Put request with waled body and return 200', ()=>{
- //   cy.updateGoal().then((response) => {
- //        expect(response.status).to.equal(200)
- //     })
- //   })
+        // 2. Перевірити, що ціль створено правильно
+        cy.getGoal().then(getResponse => {
+            cy.log('Verified created goal:', getResponse.body);
+        });
 
-     // it ('sound not be able deleted goal', () => {
-     //     cy.createGoal();
-     //     cy.deleteGoal();
-     //     cy.updateGoal().then((response) =>{expect((response.status).to.equal(404))})
-     // })
+        // 3. Оновити ціль
+        cy.updateGoal().then(updateResponse => {
+            cy.log('Goal updated');
+        });
 
-     // it('should not be able to update deleted goal', () => {
-     //     cy.createGoal().then(() => {
-     //         cy.deleteGoal().then(() => {
-     //             cy.updateGoal().then((response) => {
-     //                 expect(response.status).to.equal(404);
-     //             });
-     //         });
-     //     });
-     // });
-     it('test createGoal', () => {
-         cy.createGoal().then((response) => {
-             cy.log(JSON.stringify(response.body));
-             expect(response.status).to.equal(200);
-         });
-     });
-  })
+        // 4. Перевірити, що оновлення відбулося
+        cy.verifyUpdatedGoal().then(verifyUpdated => {
+            cy.log('Verified updated goal');
+        });
+
+        // 5. Видалити ціль
+        cy.deleteGoal().then(deleteResponse => {
+            cy.log('Goal deleted');
+        });
+
+        // 6. Перевірити, що ціль більше не існує
+        cy.verifyDeletedGoal().then(deletedCheck => {
+            cy.log('Verified goal no longer exists');
+        });
+    });
+});
 
 
 
--- ClickUp_JS/cypress/support/commands.js
+++ ClickUp_JS/cypress/support/commands.js
@@ -24,14 +24,13 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { .
 import { faker } from '@faker-js/faker';
-import './commands'
 
 Cypress.Commands.add('sentRequest', (endpoint, method, body=null) => {
     cy.request({
         url: endpoint,
         method: method,
         headers:{
-            'Authorization': 'pk_188686871_GUMKJPKLN6NVIKT6XYQ4F9V7VT1X6TAC',
+            'Authorization': 'pk_200447320_BF704GUZ6OPFSQ08L2NN2BWLXZEU0IZM',
             'Content-Type': 'application/json',
         },
         failOnStatusCode: false,
@@ -40,32 +39,70 @@ Cypress.Commands.add('sentRequest', (endpoint, method, body=null) => {
 })
 
 Cypress.Commands.add('createGoal', () => {
-    return cy.sentRequest('/team/90151137047/goal', 'POST', {
-        name: faker.internet.email()
-    }).then((response) => {
-        cy.log(JSON.stringify(response.body)); // ← додай для дебагу
-        console.log(response.body);           // ← вивід у devtools
+    const name = faker.company.name();
+    cy.wrap(name).as('goalName');
 
-        // Перевір, чи існує response.body.goal і goal.id
-        if (response.body && response.body.goal && response.body.goal.id) {
-            cy.wrap(response.body.goal.id).as('goalId');
-        } else {
-            throw new Error('goal.id не знайдено у відповіді: ' + JSON.stringify(response.body));
-        }
+    return cy.sentRequest('/team/90151221188/goal', 'POST', { name }).then(response => {
+        expect(response.status).to.be.oneOf([200, 201]);
+        expect(response.body).to.have.property('goals');
+        expect(response.body.goals[0]).to.have.property('id');
+        expect(response.body.goals[0].name).to.equal(name);
 
+        cy.wrap(response.body.goals[0].id).as('goalId');
         return response;
     });
 });
 
-Cypress.Commands.add('updateGoal', () => {
-    cy.get('@goalId').then((id)=>{
-        cy.sentRequest('/goal/' + id, 'PUT', {'name': faker.company.name()})
-    })
-})
+Cypress.Commands.add('getGoal', () => {
+    return cy.get('@goalId').then(id => {
+        return cy.sentRequest(`/goal/${id}`, 'GET').then(response => {
+            expect(response.status).to.equal(200);
+            cy.get('@goalName').then(name => {
+                expect(response.body.name).to.equal(name);
+            });
+            return response;
+        });
+    });
+});
 
-Cypress.Commands.add('createAndUpdateGoalPositive', () => {
-    cy.createGoal()
-    cy.get('@goalId').then((id)=> {
-        cy.updateGoal()
-    })
-})
+Cypress.Commands.add('updateGoal', () => {
+    const newName = faker.company.name();
+    cy.wrap(newName).as('updatedName');
+
+    return cy.get('@goalId').then(id => {
+        return cy.sentRequest(`/goal/${id}`, 'PUT', { name: newName }).then(response => {
+            expect(response.status).to.be.oneOf([200, 204]);
+            return response;
+        });
+    });
+});
+
+Cypress.Commands.add('verifyUpdatedGoal', () => {
+    return cy.get('@goalId').then(id => {
+        return cy.sentRequest(`/goal/${id}`, 'GET').then(response => {
+            expect(response.status).to.equal(200);
+            cy.get('@updatedName').then(updatedName => {
+                expect(response.body.name).to.equal(updatedName);
+            });
+            return response;
+        });
+    });
+});
+
+Cypress.Commands.add('deleteGoal', () => {
+    return cy.get('@goalId').then(id => {
+        return cy.sentRequest(`/goal/${id}`, 'DELETE').then(response => {
+            expect([200, 204]).to.include(response.status);
+            return response;
+        });
+    });
+});
+
+Cypress.Commands.add('verifyDeletedGoal', () => {
+    return cy.get('@goalId').then(id => {
+        return cy.sentRequest(`/goal/${id}`, 'GET').then(response => {
+            expect([404, 400, 410]).to.include(response.status);
+            return response;
+        });
+    });
+});
